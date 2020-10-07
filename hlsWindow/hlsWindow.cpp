2281d35c91274d13ebc3fe798e512a64490a41d7
colinporth <colinporth@users.noreply.github.com>
2020-10-07 12:16:36 +0100

    not stdfax in hlswindow

--- hlsWindow/hlsWindow.cpp
+++ hlsWindow/hlsWindow.cpp
@@ -1,6 +1,21 @@
  // hlsWindow.cpp
 //{{{  includes
-#include "stdafx.h"
+#define _CRT_SECURE_NO_WARNINGS
+#define _SILENCE_CXX17_UNCAUGHT_EXCEPTION_DEPRECATION_WARNING
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <windows.h>
+#include <wrl.h>
+
+#include <d3d11.h>
+#include <d3d11_1.h>
+#include <d2d1.h>
+#include <d2d1_1.h>
+#include <d2d1helper.h>
+#include <DXGI1_2.h>
+#include <dwrite.h>
+
 #include <shellapi.h>
 
 #include "../common/cD2dWindow.h"
